28d6c29236ebedd7ac539f16624a11da3d18d7cd
jimmy-guzman <hi@jimmyguzman.com>
2021-04-18 19:30:56 -0500

    refactor: ♻️ encapsulate state init into provider

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,18 +1,14 @@
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { store, persistor } from '@recipe-box/state'
+import { StateProvider } from '@recipe-box/state'
 
 import { App } from './app'
 
 render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <App />
-      </Router>
-    </PersistGate>
-  </Provider>,
+  <StateProvider>
+    <Router>
+      <App />
+    </Router>
+  </StateProvider>,
   document.getElementById('app')
 )
