745a752052c601051885606de22899c47b6fb197
jimmy-guzman <hi@jimmyguzman.com>
2023-12-16 14:17:07 -0600

    test: ✅ migrate from `cypress` to `playwright`

---
+++ apps/client/e2e/recipes.spec.ts
@@ -0,0 +1,17 @@
+/* eslint-disable jest/no-done-callback */
+/* eslint-disable jest/expect-expect */
+import { expect, test } from '@playwright/test'
+
+test('should be able to create new recipe', async ({ page }) => {
+  await page.goto('/')
+
+  await page
+    .getByRole('textbox', { name: /new recipe/ })
+    .fill('banana strawberry smoothie')
+  await page.getByRole('textbox', { name: /new recipe/ }).press('Enter')
+  await page.getByRole('link', { name: 'banana strawberry smoothie' }).click()
+
+  await expect(
+    page.getByRole('textbox', { name: /new ingredient/ })
+  ).toBeVisible()
+})
